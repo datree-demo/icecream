db69b5bc71ad64698675efe5445b1533f0ab4359
Shimon Tolts <shimon@datree.io>
2019-11-30 18:59:15 -0800

    fail only on critical

--- .github/workflows/getECRVln.sh
+++ .github/workflows/getECRVln.sh
@@ -9,4 +9,4 @@ echo "Waiting for AWS ECR Image Scanning..."
 sleep 5
 echo "Waiting for AWS ECR Image Scanning..."
 sleep 5
-aws ecr describe-image-scan-findings --repository-name icecream --image-id imageTag=$IMAGE_TAG --region us-west-1 |jq '.imageScanFindings.findings[] | {severity}'|grep 'HIGH\|CRITICAL' && echo "Found Critical or High Vlnerabilities" && exit 1 || echo "no Critical or High Vlnerabilities found" && exit 0
+aws ecr describe-image-scan-findings --repository-name icecream --image-id imageTag=$IMAGE_TAG --region us-west-1 |jq '.imageScanFindings.findings[] | {severity}'|grep 'CRITICAL' && echo "Found Critical Vlnerabilities" && exit 1 || echo "no Critical Vlnerabilities found" && exit 0
