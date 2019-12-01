78cea55e762a0cf32a334823062cd1e1a2134cc5
Shimon Tolts <shimon@datree.io>
2019-11-30 17:58:30 -0800

    ecr

---
+++ .github/workflows/getECRVln.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+IMAGE_TAG=$1
+echo "Checking Vlnerabilities for: $IMAGE_TAG"
+echo "Waiting for AWS ECR Image Scanning..."
+sleep 5
+echo "Waiting for AWS ECR Image Scanning..."
+sleep 5
+echo "Waiting for AWS ECR Image Scanning..."
+sleep 5
+echo "Waiting for AWS ECR Image Scanning..."
+sleep 5
+aws ecr describe-image-scan-findings --repository-name icecream --image-id imageTag=$IMAGE_TAG --region us-west-1 |jq '.imageScanFindings.findings[] | {severity}'|grep 'HIGH\|CRITICAL' && echo "Found Critical or High Vlnerabilities!" && exit 1 || echo "no Critical or High Vlnerabilities found!" && exit 0
